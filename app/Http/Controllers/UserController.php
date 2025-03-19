b803765d047555dfadeb5dc817543edee13ef78e
Cilin <50434281+cilinjohnrey@users.noreply.github.com>
2025-03-19 14:18:56 +0800

    Implement get all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,4 +13,29 @@ class UserController extends Controller
 
         return response()->json(['users' => $users]);
     }
+
+    public function addUser(Request $request){
+        $request->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'middle_name' => ['nullable', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string'],
+            'confirm_password' => ['required', 'same:password'],
+            'role_id' => ['required', 'exists:roles,id'],
+            'user_status_id' => ['required', 'exists:user_statuses,id'],
+        ]);
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role_id' => $request->role_id,
+            'user_status_id' => $request->user_status_id,
+        ]);
+
+        return response()->json(['message' => 'User successfully created!', 'user' => $user]);
+    }
 }
